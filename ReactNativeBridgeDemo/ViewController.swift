008d0236f5420387e154464e7ff80028512e80aa
Levy, Dvir [ICG-IT NE] <dl20932@imceu.eu.ssmb.com>
2018-02-15 08:42:13 +0200

    Initial Commit

---
+++ ReactNativeBridgeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactNativeBridgeDemo
+//
+//  Created by dl20932 on 15/02/2018.
+//  Copyright © 2018 dl20932. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
